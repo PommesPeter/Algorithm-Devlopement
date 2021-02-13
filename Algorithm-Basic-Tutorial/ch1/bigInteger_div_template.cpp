8b7f3bc33f5151d669cefce1dce5019a7f17d3e9
PommesPeter <434596665@qq.com>
2021-02-14 01:04:46 +0800

    modify code of div

--- Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
+++ Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
@@ -8,31 +8,22 @@
 
 using namespace std;
 
-vector<int> div(vector<int> &A, int &b) {
+vector<int> div(vector<int> &A, int &b, int &r) {
 
     vector<int> C;
-    int t = 0;
 
-    for (int i = 0; i < A.size() || t != 0; i++) {
-        if (i < A.size()) t += (A[i] * b);
-        C.push_back(t % 10);
-        t /= 10;
-    }
-    while (C.size() > 1 && C.back() == 0)   C.pop_back();
     return C;
 }
 
 int main() {
 
     string a;
-    int b;
+    int b, r;
     vector<int> A;
     cin >> a >> b;
-    for (int i = a.length() - 1; i >= 0; i--) {
-        A.push_back(a[i] - '0');
-    }
-    auto C = mul(A, _b);
+    for (int i = a.length() - 1; i >= 0; i--) A.push_back(a[i] - '0');
+    auto C = div(A, b, r);
     for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
-
+    cout << endl << r << endl;
     return 0;
 }
