26c4351f8575a3d906310a6161efd66f6dc2785d
PommesPeter <434596665@qq.com>
2021-03-21 22:20:44 +0800

    update

--- Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
+++ Algorithm-Basic-Tutorial/ch1/bigInteger_div_template.cpp
@@ -5,13 +5,22 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> div(vector<int> &A, int &b, int &r) {
 
     vector<int> C;
-
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; i--) {
+        r = r * 10 + A[i];
+        C.push_back(r % b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end());
+    //去除前导0
+    while (C.size() > 1 && C.back() == 0)   C.pop_back();
     return C;
 }
 
